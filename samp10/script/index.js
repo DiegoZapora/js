10ab8e7825935296d14b9aa57864730602e77bc7
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-05-08 12:53:57 -0300

    ghgh

--- samp10/script/index.js
+++ samp10/script/index.js
@@ -1,17 +1,27 @@
-function resul() {
-    const num1 = document.getElementById("num1")
-    const num2 = document.getElementById("num2")
-    const num3 = document.getElementById("num3")
-    const num4 = document.getElementById("num4")
-    const num5 = document.getElementById("num5")
-    const num6 = document.getElementById("num6")
-    const num7 = document.getElementById("num7")
-    const num8 = document.getElementById("num8")
-    const num9 = document.getElementById("num9")
-    const num10 = document.getElementById("num10")
-    const div = document.getElementById("div")
-    const vez = document.getElementById("vez")
-    const meno = document.getElementById("meno")
-    const mais = document.getElementById("mais")
-    const res = document.getElementById("res")
-}
+let resultado = document.getElementById('res');
+let limparVisor = true;
+
+document.querySelectorAll('.num').forEach(botao => {
+    botao.addEventListener('click', () => {
+        let valor = botao.value;
+
+        if (limparVisor) {
+            res.innerText = '';
+            limparVisor = false;
+          }
+
+        if (valor === '=') {
+            try {
+                resultado.innerText = eval(resultado.innerText);
+                limparVisor = true;
+            } catch {
+                resultado.innerText = 'Erro';
+                limparVisor = true;
+            }
+        } else if (valor === 'C') {
+            resultado.innerText = '';
+        } else {
+            resultado.innerText += valor;
+        }
+    });
+});
